4adce6b2c3c560222647c401ba89886219df74bc
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2026-04-23 09:49:33 -0400

    chore: point formula at rekody/rekody after org move

--- Formula/rekody.rb
+++ Formula/rekody.rb
@@ -1,15 +1,15 @@
 class Rekody < Formula
   desc "Open-source, privacy-first voice dictation for the terminal"
-  homepage "https://github.com/tonykipkemboi/rekody"
+  homepage "https://github.com/rekody/rekody"
   version "0.5.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tonykipkemboi/rekody/releases/download/v#{version}/rekody-#{version}-macos-aarch64.tar.gz"
+      url "https://github.com/rekody/rekody/releases/download/v#{version}/rekody-#{version}-macos-aarch64.tar.gz"
       sha256 "d22527a4a673a4c90e64fc81932c7d98778fb5e0dc404633f73c43608d93c349"
     else
-      url "https://github.com/tonykipkemboi/rekody/releases/download/v#{version}/rekody-#{version}-macos-x86_64.tar.gz"
+      url "https://github.com/rekody/rekody/releases/download/v#{version}/rekody-#{version}-macos-x86_64.tar.gz"
       sha256 "e3df6692344100ee36a6da872803abc0e5f39c31bcbbb2ff0c6e71abfd09888c"
     end
   end
